d1bd7b512beeaf8a817937bdd4a62b737bcdcfd9
Roy Duineveld <royduineveld@gmail.com>
2021-07-20 14:22:12 +0200

    Sortorder

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Rapidez\Core\RapidezFacade as Rapidez;
 use Rapidez\MagePrinceFaq\Models\Group;
 
 Route::get(Rapidez::config('faqtab/seo/faq_url', 'faq'), function () {
-    $categories = Group::with('faqs')->get();
+    $categories = Group::with(['faqs' => fn ($query) => $query->orderBy('sortorder')])->orderBy('sortorder')->get();
     return view('mageprincefaq::overview', compact('categories'));
 });
