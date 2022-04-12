bad0b9ac3bb1ca2756e704d1046365fd3457a94c
Roy Duineveld <royduineveld@gmail.com>
2022-04-12 16:21:11 +0200

    Use the new core facade location

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Rapidez\Core\RapidezFacade as Rapidez;
+use Rapidez\Core\Facades\Rapidez;
 use Rapidez\MagePrinceFaq\Models\Group;
 
 Route::get(Rapidez::config('faqtab/seo/faq_url', 'faq'), function () {
