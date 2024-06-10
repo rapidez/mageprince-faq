6d833aad6bc264cc0ff5fe86d4e3561789b7faf1
Bob Wezelman <bob@justbetter.nl>
2024-06-10 09:27:59 +0200

    Fix broken packages:discover without database (#6)

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use Rapidez\Core\Facades\Rapidez;
 use Rapidez\MagePrinceFaq\Models\Group;
+use Illuminate\Database\QueryException;
 
-Route::get(Rapidez::config('faqtab/seo/faq_url', 'faq'), function () {
+try {
+    $url = Rapidez::config('faqtab/seo/faq_url', 'faq');
+} catch(QueryException $e) {
+    $url = 'faq';
+}
+
+Route::get($url, function () {
     $categories = Group::with(['faqs' => fn ($query) => $query->orderBy('sortorder')])->orderBy('sortorder')->get();
 
     return view('mageprincefaq::overview', compact('categories'));
